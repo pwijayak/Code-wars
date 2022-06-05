b5750db86096e1a26d8a7bbe9ba93054501249f7
Prasanna Wijayakoon <praswijayakoon@gmail.com>
2022-06-05 08:29:03 -0400

    added solution 7

--- js/solution006.js
+++ js/solution006.js
@@ -4,13 +4,25 @@ function getSum( a,b )
   
 //   for loop
 //   from a + 1 to the end
-
+  
+if (a<b){
 let answer=[]
 for(let i=a; i<=b; i++){
   answer.push(i)
   
-} console.log(answer)
-console.log(answer.reduce((acc,ind)=>acc+ind,0)) 
+}
+return answer.reduce((acc,ind)=>acc+ind,0)
 }
 
-getSum(1,5)
+else if (b<a){
+  let answer=[]
+for(let i=b; i<=a; i++){
+  answer.push(i)
+  
+}
+return answer.reduce((acc,ind)=>acc+ind,0)
+}
+
+else if (a==b){
+  return a
+}}
