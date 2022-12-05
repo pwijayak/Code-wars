52e2308068db3c7467adf83704ba0623e00cec1a
pwijayak <59058784+pwijayak@users.noreply.github.com>
2022-12-05 13:47:00 -0500

    Create solution116.js

---
+++ js/solution116.js
@@ -0,0 +1,6 @@
+//return the total number of smiling faces in the array
+function countSmileys(arr) {
+  
+let y= arr.filter((x)=> x===':)' || x===':D' || x=== ';-D' || x===':~)'|| x===':-)'||x ===';~)'||x===':-D'||x===':~D'|| x===';)'||x===';~D'||x===';D'||x===';-)')
+return y.length
+}
